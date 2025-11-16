4bbfed6656bc096a5613a58d2ea44343fe351589
JakubHaran <jakub.haran02@gmail.com>
2025-11-16 14:07:47 +0100

    bug fix

--- static/js/register.js
+++ static/js/register.js
@@ -7,3 +7,10 @@ const layout = document.querySelector(".layout");
 
 if (layout.getBoundingClientRect() <= 524)
   window.addEventListener("scroll", show);
+const register_form = document.querySelector(".register-form");
+
+register_form.addEventListener("submit", () => {
+  const register_btn = document.querySelector(".signup-btn");
+  register_btn.disabled = true;
+  register_btn.textContent = "Saving...";
+});
