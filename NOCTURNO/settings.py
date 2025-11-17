1cdc8ec453b55b433e17577812dfa330f7c8b89c
JakubHaran <jakub.haran02@gmail.com>
2025-11-17 11:48:04 +0100

    bug fix

--- NOCTURNO/settings.py
+++ NOCTURNO/settings.py
@@ -103,15 +103,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media (Cloudflare R2)
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = os.environ("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "nocturno-media"
 
 AWS_S3_REGION_NAME = "auto"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_ADDRESSING_STYLE = "virtual"
 
-AWS_S3_ENDPOINT_URL = os.environ("AWS_S3_ENDPOINT_URL")
+AWS_S3_ENDPOINT_URL = os.environ["AWS_S3_ENDPOINT_URL"]
 
 AWS_S3_CUSTOM_DOMAIN = "media.nocturno.click"
 MEDIA_URL = "https://media.nocturno.click/"
