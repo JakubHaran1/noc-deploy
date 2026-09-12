f5b9b105d109154c18379d052cb26aa9cb34e43a
jakubharan1 <jakub.haran02@gmail.com>
2026-09-12 18:14:17 +0200

    add connection to  bucket s3

--- NOCTURNO/settings.py
+++ NOCTURNO/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": os.environ["DB_HOST"],
         "PORT": os.environ.get("DB_PORT", "5432"),
-    }
+    },
 }
 
 
@@ -99,10 +99,8 @@ USE_TZ = True
 
 
 # lub z custom domain:
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-
-# ------------------------
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_STORAGE_BUCKET_NAME = "nocturno-bucket"
 AWS_S3_REGION_NAME = "eu-north-1"
