a4f2772a7f5742b900158f15f72bf982ee0e7e7c
jakubharan1 <jakub.haran02@gmail.com>
2026-09-13 04:13:51 +0200

    connect to resend api

--- NOCTURNO/settings.py
+++ NOCTURNO/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # ------------------------
 # Podstawowe ustawienia
