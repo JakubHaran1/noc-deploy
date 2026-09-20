0fb6e770f1d822257c0c146bed9798ce20bc9724
jakubharan1 <jakub.haran02@gmail.com>
2026-09-20 04:49:45 +0200

    bug-fix

--- APP/views.py
+++ APP/views.py
@@ -216,7 +216,7 @@ class RegisterView(views.View):
             msg = EmailMultiAlternatives(
                 subject=mail_subject,
                 body=html_txt,
-                from_email="noreply@nocturno.cloud.haran",
+                from_email="noreply@nocturno.haran.cloud",
                 to=recipient_list,
             )
 
